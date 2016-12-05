db7ad52928c9a5589443b96d0709404c055e7904
Preston Edmands <pedmands@gmail.com>
2016-12-05 07:25:13 -0500

    Built basic structure for search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBw3rJxO9rWa6YYj8NA10F7AHmETp8hicc';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+    <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
