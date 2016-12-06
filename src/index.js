b398246c5343760d30b806cba452b0348d9ff852
Preston Edmands <pedmands@gmail.com>
2016-12-05 23:29:38 -0500

    Installed lodash to throttle API requests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBw3rJxO9rWa6YYj8NA10F7AHmETp8hicc';
 
@@ -11,21 +12,34 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_KEY, term: 'lego puzzle'}, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('lego puzzles');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
       <div>
-      <SearchBar />
-      <VideoList videos={this.state.videos} />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
-}
+} // App
 
 ReactDOM.render(<App />, document.querySelector('.container'));
